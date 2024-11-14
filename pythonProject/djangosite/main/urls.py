88d40ce16a4ec8b41cf131cc95f2ae94b503be4c
unknown <energiguy@gmail.com>
2024-11-14 17:53:31 +0300

    search field added

--- pythonProject/djangosite/main/urls.py
+++ pythonProject/djangosite/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.mainpage, name = 'home'),
-    path('profile', views.profile, name = 'profile')
+    path('profile', views.profile, name = 'profile'),
+    path('search/', views.search_view, name='search')
 ]
--- pythonProject/djangosite/main/views.py
+++ pythonProject/djangosite/main/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import Course
+from django import forms
+from .forms import SearchForm
 
 def mainpage(request):
-    return render(request, '')
+    return render(request, 'main/search.html')
 
 def profile(request):
     return render(request, '')
+
+def search_view(request):
+    form = SearchForm(request.GET or None)
+    query = None
+    results = []
+
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        results = Course.objects.filter(title__icontains=query) | Course.objects.filter(description__icontains=query)
+
+    return render(request, 'search.html', {'form': form, 'query': query, 'results': results})
